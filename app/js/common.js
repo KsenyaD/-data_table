fef3e7ccd356d8cf7b82893c83421a8277f36b5e
Ксения <ksenya.zaharova92@gmail.com>
2020-06-09 17:13:43 +0300

    validate form

--- app/js/common.js
+++ app/js/common.js
@@ -1,38 +1,37 @@
 let arrayPersons = [];
 const form = document.querySelector(".form-add-str");
+const wrap = document.querySelector(".wrap");
 
 function displayTable(array) { // отрисовка таблицы
     const tableArr = ['<table class="table">'];
     tableArr.push('<tr><th scope="col" class="th">ID</th> <th scope="col" colspan = "2" class="th">Name</th><th scope="col" class="th">Age</th> <th></th></tr>');
     tableArr.push('<tr><td></td> <td><button class="sort-button" onclick=sortArrayAscending("Surname")><img src="./svg/sort_ascending.png"></button> <button class="sort-button" onclick=sortArrayDownward("Surname")><img src="./svg/sort_descending.png"></button></td> ' +
         '<td><button class="sort-button" onclick=sortArrayAscending("Name")><img src="./svg/sort_ascending.png"></button><button class="sort-button" onclick=sortArrayDownward("Name")><img src="./svg/sort_descending.png"></button></td> ' +
-        '<td><button class="sort-button" onclick=sortArrayAscending("Age")><img src="./svg/sort_ascending.png"></button><button class="sort-button" onclick=sortArrayDownward("Age")><img src="./svg/sort_descending.png"></button></td><td colspan = "2"><button class="add-button" onclick=addString() >Add</button></td></tr>');
+        '<td><button class="sort-button" onclick=sortArrayAscending("Age")><img src="./svg/sort_ascending.png"></button><button class="sort-button" onclick=sortArrayDownward("Age")><img src="./svg/sort_descending.png"></button></td><td colspan = "2"><button class="add-button" onclick=showDialog() >Add</button></td></tr>');
 
     array.forEach((obj) => {
         tableArr.push(`<tr><td>${obj.ID}</td> <td>${obj.Surname}</td><td>${obj.Name}</td> <td>${obj.Age}</td> <td><button>edit</button></td><td><button id="${obj.ID}" onclick=deleteString(${obj.ID})>Delete</button></td></tr>`);
     });
 
     tableArr.push('</table>');
-    document.body.innerHTML = tableArr.join('\n')
+    wrap.innerHTML = tableArr.join('\n')
 }
 
-function checkForUniqueValue(input) { //сразвнение введенного id на уникальность
+function checkForUniqueValue(input) {
     for (let i = 0; i < arrayPersons.length; i++) {
-        let value = Number(arrayPersons[i]["ID"]);
+        let value = arrayPersons[i].ID;
         if (value === input.value) {
-            alert("ID не уникально")
+            input.value = " ";
+            alert("ID не уникально");
             break
         }
     }
+} //сразвнение введенного id на уникальность
 
-}
 
-
-function addString() { // добавление строчки
-
+function showDialog() {
     form.classList.add("form-add-str__active")
-
-}
+} // добавление строчки
 
 
 function sortArrayAscending(value) {
@@ -82,7 +81,7 @@ function sortArrayDownward(value) {
                 return -1;
             if (nameA < nameB)
                 return 1;
-            return 0 // Никакой сортировки
+            return 0
         })
     }
 
@@ -94,7 +93,7 @@ function sortArrayDownward(value) {
                 return -1;
             if (nameA < nameB)
                 return 1;
-            return 0 // Никакой сортировки
+            return 0
         })
     }
 
@@ -102,22 +101,22 @@ function sortArrayDownward(value) {
     displayTable(arrayPersons);
 } //сортировка по убыванию
 
-function deleteString(id) { // удаление строчки
+function deleteString(id) {
     for (let i = 0; i < arrayPersons.length; i++) {
-        let value = Number(arrayPersons[i]["ID"]);
+        let value = Number(arrayPersons[i].ID);
         if (value === id) {
             arrayPersons.splice(i, 1);
             break
         }
     }
     displayTable(arrayPersons);
-}
+} // удаление строчки
 
-function removeNumbers(input) { //удаление символов после запятой
+function removeNumbers(input) {
     if (input.value.indexOf(".") != '-1') {
         input.value = input.value.substring(0, input.value.indexOf(".") + 5);
     }
-}
+} //удаление символов после запятой
 
 async function downloadData() { // получение и преобразование json
 
